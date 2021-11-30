c3993433e21b916770173130a692ffae81b76e4d
Simon Longy <slongy@iutbayonne.univ-pau.fr>
2021-11-30 16:12:31 +0100

    Ajout de plusieurs pages au contrôleur métier (fin du TP1)

--- src/Controller/ProstagesController.php
+++ src/Controller/ProstagesController.php
@@ -11,10 +11,34 @@ class ProstagesController extends AbstractController
     /**
      * @Route("/", name="prostages_accueil")
      */
-    public function index(): Response
+    public function afficherPageAccueil(): Response
     {
-        return $this->render('prostages/index.html.twig', [
-            'controller_name' => 'ProstagesController',
+        return $this->render('prostages/affichageAccueil.html.twig');
+    }
+	
+	/**
+     * @Route("/entreprises", name="prostages_entreprises")
+     */
+    public function afficherPageEntreprises(): Response
+    {
+        return $this->render('prostages/affichageEntreprises.html.twig');
+    }
+	
+	/**
+     * @Route("/formations", name="prostages_formations")
+     */
+    public function afficherPageFormations(): Response
+    {
+        return $this->render('prostages/affichageFormations.html.twig');
+    }
+	
+	/**
+     * @Route("/stages/{id}", name="prostages_stages")
+     */
+    public function afficherPageStages($id): Response
+    {
+        return $this->render('prostages/affichageStages.html.twig', [
+            'idStage' => $id,
         ]);
     }
 }
