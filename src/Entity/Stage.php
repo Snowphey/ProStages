a85a17eb6f51184ef334f79ba71a091a0747cfb2
Simon Longy <slongy@iutbayonne.univ-pau.fr>
2021-12-16 10:55:15 +0100

    Création de l'entité Stage et du fichier .env.local

---
+++ src/Entity/Stage.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StageRepository::class)
+ */
+class Stage
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $descMissions;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $emailContact;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?int
+    {
+        return $this->code;
+    }
+
+    public function setCode(int $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescMissions(): ?string
+    {
+        return $this->descMissions;
+    }
+
+    public function setDescMissions(string $descMissions): self
+    {
+        $this->descMissions = $descMissions;
+
+        return $this;
+    }
+
+    public function getEmailContact(): ?string
+    {
+        return $this->emailContact;
+    }
+
+    public function setEmailContact(string $emailContact): self
+    {
+        $this->emailContact = $emailContact;
+
+        return $this;
+    }
+}
